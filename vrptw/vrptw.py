65e1becfa16fdea6766a6a6b23805a24f6225e74
Bjørn Petersen <bjorn@flowty.ai>
2023-07-24 15:00:24 +0200

    Read instances from CVRPLIB

--- vrptw/vrptw.py
+++ vrptw/vrptw.py
@@ -1,35 +1,14 @@
 # Vehicle Routing Problem with Time Windows
 import sys
 from flowty import Model, xsum
+from fetch_vrptw import fetch
 from or_datasets import vrp_rep
 
-bunch = vrp_rep.fetch_vrp_rep("solomon-1987-r1", instance="R102_025")
-name, n, E, c, d, Q, t, a, b, x, y = bunch["instance"]
+name, n, E, c, d, Q, t, a, b, x, y = fetch("Solomon", "R101", 25)
+# bunch = vrp_rep.fetch_vrp_rep("solomon-1987-r1", instance="R101_025")
+# name, n, E, c, d, Q, t, a, b, x, y = bunch["instance"]
 
 m = Model()
-
-# Get reduced number of customers from cmd line
-if len(sys.argv) > 1:
-    numCustomer = int(sys.argv[1])
-
-    print(f"Number of customers: {numCustomer}")
-
-    # strip customers
-    old_n = n
-    n = numCustomer + 2
-    data = [
-        ((e[0], e[1]), c[i], t[i])
-        for i, e in enumerate(E)
-        if (e[0] < n - 1 and e[1] < n - 1) or (e[0] < n - 1 and e[1] == old_n - 1)
-    ]
-    E = [e[0] for e in data]
-    E = [e if e[1] != old_n - 1 else (e[0], n - 1) for e in E]
-    c = [e[1] for e in data]
-    t = [e[2] for e in data]
-
-    d = d[: n - 1] + [d[-1]]
-    a = a[: n - 1] + [a[-1]]
-    b = b[: n - 1] + [b[-1]]
 
 # one graph, it is identical for all vehicles
 g = m.addGraph(obj=c, edges=E, source=0, sink=n - 1, L=1, U=n - 2, type="B")
@@ -46,11 +25,11 @@ m.addResourceDisposable(
 )
 
 # set partition constriants
-for i in range(n)[1:-1]:
+for i in range(1, n-1):
     m += xsum(x * 1 for x in g.vars if i == x.source) == 1
 
 # packing set
-for i in range(n)[1:-1]:
+for i in range(1, n-1):
     m.addPackingSet([x for x in g.vars if i == x.source])
 
 status = m.optimize()
